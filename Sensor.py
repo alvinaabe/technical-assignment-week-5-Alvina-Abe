6a474ae2078c107af32121cbce88f7301b79ed48
alvina <abealvina5@gmail.com>
2022-07-29 14:30:08 +0700

    up

--- Sensor.py
+++ Sensor.py
@@ -1,57 +1,7 @@
-import RPi.GPIO as GPIO
-import time
-import sys
-from hx711 import HX711
-
-# Force Python 3 ###########################################################
-
-if sys.version_info[0] != 3:
-    raise Exception("Python 3 is required.")
-
-############################################################################
-GPIO.setwarnings(False)
-
-hx = HX711(5, 6)
+# 3Sebuah script utama main.py yang terdiri dari sebuah logic sederhana yang menjawab sebuah use case sederhana ( jelaskan use case tersebut dalam MD files! )
 
 
-def cleanAndExit():
-    print("Cleaning...")
-    GPIO.cleanup()
-    print("Bye!")
-    sys.exit()
-
-
-def setup():
-    """
-    code run once
-    """
-    #Pasted Offset and Scale I got from calibration..
-    hx.set_offset(8608276.3125)
-    hx.set_scale(19.828315054835493)
-
-
-def loop():
-    """
-    code run continuosly
-    """
-   
-    try:
-
-        val = hx.get_grams()
-        print(val)
-        hx.power_down()
-        time.sleep(0.001)
-        hx.power_up()
-
-
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
-
-
-##################################
-
-if __name__ == "__main__":
-
-    setup()
-    while True:
-        loop()
+from gpiozero import LightSensor
+ldr = LightSensor(4)
+while True:
+	print(ldr.value)
